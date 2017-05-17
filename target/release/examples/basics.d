af9cad89a1bef9bc09495aa35a96813967b56624
Steve Jenson <stevej@buoyant.io>
2017-05-16 21:53:34 -0700

    forgot to ignore target. noob move

--- target/release/examples/basics.d
+++
@@ -1,1 +0,0 @@
-/home/stevej/src/rust-experiments/target/release/examples/basics: /home/stevej/src/rust-experiments/examples/basics.rs /home/stevej/src/rust-experiments/src/lib.rs
--- target/release/examples/fold_nums.d
+++
@@ -1,1 +0,0 @@
-/home/stevej/src/rust-experiments/target/release/examples/fold_nums: /home/stevej/src/rust-experiments/examples/fold_nums.rs /home/stevej/src/rust-experiments/src/lib.rs
